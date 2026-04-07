44689093d45476711f54a49e89016349c8e0b8c6
David Scheutz <dscheutz@keeta.com>
2026-04-07 10:10:09 -0500

    publish result last block hash helper

--- Sources/KeetaClient/Networking/PublishResult.swift
+++ Sources/KeetaClient/Networking/PublishResult.swift
@@ -1,5 +1,9 @@
 import BigInt
 
+public enum PublishResultError: Error {
+    case feeBlockMissingInStaple
+}
+
 public struct PublishResult {
     public struct PaidFee {
         public let amount: BigInt
@@ -23,5 +27,23 @@ public struct PublishResult {
             result[fee.token, default: 0] += fee.amount
         }
         return result
+    }
+    
+    public func lastBlockHash(for account: Account? = nil) throws -> String? {
+        if let feeBlockHash {
+            guard let feeBlock = staple.blocks.first(where: { $0.hash == feeBlockHash }) else {
+                throw PublishResultError.feeBlockMissingInStaple
+            }
+            
+            if account == nil || feeBlock.rawData.account.publicKeyString == account?.publicKeyString {
+                return feeBlockHash
+            }
+        }
+        
+        if let account {
+            return staple.blocks.last { $0.rawData.account.publicKeyString == account.publicKeyString }?.hash
+        } else {
+            return staple.blocks.last?.hash
+        }
     }
 }
